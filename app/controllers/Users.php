01cc36e0f7c132275f05fb83bcab38d787284a43
[George.Siorokos] <[george.siorokos@soel.gr]>
2018-06-28 11:42:04 +0300

    config

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -27,4 +27,21 @@ class Users extends Controller{
             $this ->view('users/register', $data);
         }
     }
+    public function login(){
+        //Check for POST
+        if($_SERVER['REQUEST_METHOD'] == 'POST'){
+            //process form
+        }
+        else {
+            //Init data
+            $data = [
+                'email' => '',
+                'password' => '',
+                'email_err' => '',
+                'password_err' => '',
+            ];
+            // Load view
+            $this ->view('users/login', $data);
+        }
+    }
 }
