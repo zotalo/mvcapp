c294910e7d2d6b5080c236cd96c44cdb6a567643
[George.Siorokos] <[george.siorokos@soel.gr]>
2018-07-06 14:55:13 +0300

    new

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,18 +1,11 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- * Description of bootstrap
- *
- * @author Administrator
- */
 //Load Config
 require_once 'config/config.php';
+
+// Load Helpers
+require_once 'helpers/url_helper.php';
+
 //Load Libraries
 require_once 'libraries/Core.php';
 require_once 'libraries/Controller.php';
--- app/models/User.php
+++ app/models/User.php
@@ -6,9 +6,27 @@
             $this->db = new Database;
         }
 
+        //Resgister user
+        public function register($data){
+            $this->db->query('INSERT INTO users (username, email, userhashpass, userrole) VALUES (:name, :email, :password, :role)');
+            //Bind values
+            $this->db->bind(':name', $data['name']);
+            $this->db->bind(':email', $data['email']);
+            $this->db->bind(':password', $data['password']);
+            $this->db->bind(':role', 1);
+
+            //Execute
+            if($this->db->execute()){
+                return true;
+            } else {
+                return false;
+            }
+        }
+
         // Find user by email
         public function findUserByEmail($email){
             $this->db->query('SELECT * FROM users WHERE email = :email');
+            //Bind value
             $this->db->bind(':email', $email);
 
             $row = $this->db->single();
