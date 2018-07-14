75149d6ecd7fa0fd4340767efe0ab8baa74f3974
zotalo <yagoukos@gmail.com>
2018-07-14 08:47:31 +0300

    flash

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -5,11 +5,12 @@ require_once 'config/config.php';
 
 // Load Helpers
 require_once 'helpers/url_helper.php';
+require_once 'helpers/session_helper.php';
 
-//Load Libraries
-require_once 'libraries/Core.php';
-require_once 'libraries/Controller.php';
-require_once 'libraries/Database.php';
+// //Load Libraries
+// require_once 'libraries/Core.php';
+// require_once 'libraries/Controller.php';
+// require_once 'libraries/Database.php';
 
 //Autoload Core Libraries
 spl_autoload_register(function($className){
--- app/helpers/session_helper.php
+++ app/helpers/session_helper.php
@@ -1,18 +1,26 @@
 <?php 
 session_start();
 
-// Flush message helper
+// Flash message helper
 // EXAMPLE - flash ('regsiter_success', 'You are now registered');
 // DISPLAY IN VIEW - <?php echo flash('register_success');
-function flas($name = '', $message = '', $class = 'alert alert-success'){
+function flash($name = '', $message = '', $class = 'alert alert-success'){
     if(!empty ($name)){
-        if(!empty ($message) && empty($_SESSINO[$name])){
+        if(!empty ($message) && empty($_SESSION[$name])){
             if(!empty($_SESSION[$name])){
-                unset($_SESSION['name']);
+                unset($_SESSION[$name]);
             }
+            if(!empty($_SESSION[$name. '_class'])){
+                unset($_SESSION[$name. '_class']);
+            }
+
             $_SESSION[$name] = $name;
             $_SESSION[$name. '_class'] = $class;
 
+        } elseif(empty ($message) && !empty($_SESSINO[$name])){
+            $class = !empty($_SESSION[$name. '_class']) ? $_SESSION[$name. '_class'] : '';
+            echo '<div class ="'.$class.'" id="msg-flash">'.$_SESSION[$name].'</div>';
+            unset($_SESSION[$name. '_class']);
         }
     }
 }
