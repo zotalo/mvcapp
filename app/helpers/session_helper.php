a496bc3290649c95a469030b348cdd865c1f861a
zotalo <yagoukos@gmail.com>
2018-07-15 09:43:57 +0300

    session

--- app/helpers/session_helper.php
+++ app/helpers/session_helper.php
@@ -14,12 +14,12 @@ function flash($name = '', $message = '', $class = 'alert alert-success'){
                 unset($_SESSION[$name. '_class']);
             }
 
-            $_SESSION[$name] = $name;
+            $_SESSION[$name] = $message;
             $_SESSION[$name. '_class'] = $class;
 
-        } elseif(empty ($message) && !empty($_SESSINO[$name])){
+        } elseif(empty ($message) && !empty($_SESSION[$name])){
             $class = !empty($_SESSION[$name. '_class']) ? $_SESSION[$name. '_class'] : '';
-            echo '<div class ="'.$class.'" id="msg-flash">'.$_SESSION[$name].'</div>';
+            echo '<div class="'.$class.'" id="msg-flash">'.$_SESSION[$name].'</div>';
             unset($_SESSION[$name. '_class']);
         }
     }
