134a8a431904c6023fefca860eb78fdb2c9f7dd5
[George.Siorokos] <[george.siorokos@soel.gr]>
2018-07-23 10:07:04 +0300

    update

--- app/libraries/Core.php
+++ app/libraries/Core.php
@@ -1,18 +1,5 @@
 <?php
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- * Description of core
- *
- * @author Administrator
- */
-/*
- * App Core Class
+ /* App Core Class
  * Creates URL & loads core controller
  * URL FORMAT - /controller/method/params
  */
--- app/libraries/Database.php
+++ app/libraries/Database.php
@@ -1,16 +1,6 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- * Description of database
- *
- * @author Administrator
- * PDO Database Class
+/* PDO Database Class
  * Connect to database
  * Create prepared statements
  * Bind values
