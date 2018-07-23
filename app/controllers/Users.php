e7477ac580004bb046fdeae22087d713e20df5dc
[George.Siorokos] <[george.siorokos@soel.gr]>
2018-07-23 17:16:36 +0300

    session

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -129,7 +129,7 @@ class Users extends Controller{
 
                 if($loggedInUser){
                     //Create Session
-                    die('SUCCESS');
+                    $this->createUserSession($loggedInUser);
                 } else{
                     $data['password_err'] = 'Password incorrect';
                     $this->view('user/login', $data);
@@ -150,6 +150,29 @@ class Users extends Controller{
             ];
             // Load view
             $this ->view('users/login', $data);
+        }
+    }
+
+    public function createUserSession($user){
+        $_SESSION['user_id'] = $user->userid;
+        $_SESSION['user_email'] = $user->email;
+        $_SESSION['user_name'] = $user->username;
+        redirect('pages/index');
+    }
+
+    public function logout(){
+        unset($_SESSION['user_id']);
+        unset($_SESSION['user_email']);
+        unset($_SESSION['user_name']);
+        session_destroy();
+        redirect('users/login');
+    }
+
+    public function isLoggedIn(){
+        if(isset($_SESSION['user_id'])){
+            return true;
+        } else {
+            return false;
         }
     }
 }
