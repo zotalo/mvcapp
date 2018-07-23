bcc4cd4ad12699a3b20cbe0ec32b440fe19b1db9
[George.Siorokos] <[george.siorokos@soel.gr]>
2018-07-23 13:21:46 +0300

    user

--- app/models/User.php
+++ app/models/User.php
@@ -32,7 +32,6 @@
             $hashed_password = $row->password;
             if(password_verify($password, $hashed_password)){
                 return $row;
-
             } else {
                 return false;
             }
