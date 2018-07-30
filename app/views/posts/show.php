c95b3cdffa735e6c34d592b8c5c14bc89899a7cb
[George.Siorokos] <[george.siorokos@soel.gr]>
2018-07-30 15:53:53 +0300

    posts

--- app/views/posts/show.php
+++ app/views/posts/show.php
@@ -2,4 +2,7 @@
 <a href="<?php echo URLROOT; ?>/posts" class="btn btn-light"><i class="fa fa-backward"></i>Back</a>
 <br>
 <h1><?php echo $data['post']->title; ?></h1>
+<div class="bg-secondary text-white p-2 mb-3">
+    Written by <?php echo $data['user']->name; ?> on <?php echo $data['post']->created_at; ?>
+</div>
 <?php require APPROOT . '/views/inc/footer.php';?>
