11f2c42c594943fc758ee137b7969bb171f47ca5
zotalo <yagoukos@gmail.com>
2018-08-08 10:27:24 +0300

    prot

--- app/controllers/Protocols.php
+++ app/controllers/Protocols.php
@@ -1,6 +1,6 @@
 <?php
 
-Class Protocol extends Controller {
+Class Protocols extends Controller {
     public function __construct(){
         
         if(!isLoggedIn() || $_SESSION['user_role_no'] > 2){
