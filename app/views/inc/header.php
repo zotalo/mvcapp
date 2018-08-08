5603d350b420e6e4835c32827f9524df8f6e993b
zotalo <yagoukos@gmail.com>
2018-08-08 13:46:01 +0300

    fav

--- app/views/inc/header.php
+++ app/views/inc/header.php
@@ -4,7 +4,7 @@
         <meta charset="UTF-8">
         <meta name ="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <link rel="icon" href="/img/favicon.ico" type="image/x-icon"/>
+        <link rel="icon" href="<?php echo URLROOT; ?>/img/favicon.ico" type="image/x-icon"/>
         <link rel ="stylesheet" href="<?php echo URLROOT; ?>/css/style.css">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
         <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
