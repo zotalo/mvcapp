a4447f59d70f61def65a64500fd9a2e8894a8c89
zotalo <yagoukos@gmail.com>
2018-08-08 09:01:15 +0300

    protocol

--- app/controllers/Protocol.php
+++ app/controllers/Protocol.php
@@ -1,5 +1,19 @@
 <?php
 
 class Protocol extends Controller {
-    
+    public function __construct(){
+        
+        if(!isLoggedIn()){
+            redirect('users/login');
+        }
+        $this->userModel = $this->model('Protocol');
+    }
+
+    public function index(){
+        $data = [
+            'title' => 'Protocol',
+            'description' => 'Protocol for TEA'
+        ];
+        $this->view('protocol/index', $data);
+    }
 }
