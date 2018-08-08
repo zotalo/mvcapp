07d1af188ddae77b3e2e0b8ef500659224cf25fe
zotalo <yagoukos@gmail.com>
2018-08-08 14:13:40 +0300

    tea

--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -3,6 +3,7 @@
         <div class="container">
         <h1 class="display-3"><?php echo $data['title'];?></h1>
         <p class="lead"><?php echo $data['description'];?></p>
+        <div class="lead"><img src="<?php echo URLROOT;?>/img/tea.png" alt="tea" /></div>
         </div>
     </div>
 <?php require APPROOT . '/views/inc/footer.php';?>
