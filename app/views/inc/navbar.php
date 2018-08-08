b1b8da3df6349ca60dc39d49f62817d8bd5eb749
zotalo <yagoukos@gmail.com>
2018-08-08 14:11:55 +0300

    fav

--- app/views/inc/navbar.php
+++ app/views/inc/navbar.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-3">
 <div class="container">
-      <a class="navbar-brand" href="<?php echo URLROOT; ?>"><img src="<?php echo URLROOT;?>/img/tea.png" alt="tea" height="15" width="15"/><?php echo SITENAME;?></a>
+      <a class="navbar-brand" href="<?php echo URLROOT; ?>"><img src="<?php echo URLROOT;?>/img/tea.png" alt="tea" height="45" width="45"/><?php echo SITENAME;?></a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
