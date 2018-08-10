a99f025a616141e49cacd408e043c77393c54ba5
zotalo <yagoukos@gmail.com>
2018-08-10 15:08:39 +0300

    nav

--- app/views/inc/navbar.php
+++ app/views/inc/navbar.php
@@ -4,7 +4,6 @@
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-
       <div class="collapse navbar-collapse" id="navbarsExampleDefault">
         <ul class="navbar-nav mr-auto">
           <li class="nav-item ">
