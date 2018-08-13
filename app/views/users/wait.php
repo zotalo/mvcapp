8de18de8619e8f0f50f0ebcde31145d6a5b1b1c6
zotalo <yagoukos@gmail.com>
2018-08-13 15:43:13 +0300

    wait

--- app/views/users/wait.php
+++ app/views/users/wait.php
@@ -1,7 +1,7 @@
 <?php require APPROOT . '/views/inc/header.php';?>
 <div class="container row">
  <h1>
-     Αναμονή Έγκρισης από Διαχειριστή Συστήματος
+    Σε Αναμονή Έγκρισης από Διαχειριστή Συστήματος
  </h1>
 </div>
 <?php require APPROOT . '/views/inc/footer.php';?>
