c6b3387ef163fd4089703d7dcd18dda3b27c2e30
zotalo <yagoukos@gmail.com>
2018-08-13 11:13:25 +0300

    nav

--- public/js/main.js
+++ public/js/main.js
@@ -3,5 +3,16 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+jQuery("document").ready(function($){
 
+	var navigate = $('nav');
+
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 30) {
+		navigate.addClass("f-nav");
+	} else {
+		navigate.removeClass("f-nav");
+		}
+	});
+});
 
