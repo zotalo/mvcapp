be0eb5c966d3a57782fbc13595683a1e7f277fd0
zotalo <yagoukos@gmail.com>
2018-08-13 11:44:01 +0300

    adm

--- app/views/administrators/index.php
+++ app/views/administrators/index.php
@@ -1,5 +1,10 @@
 <?php require APPROOT . '/views/inc/header.php';?>
     <div class="container text-center">
+        <div class="row mb-3">
+            <div class="col-md-6">
+                    <h1>Διαχειριστής</h1>
+            </div>
+        </div> 
         <div class="row justify-content-around">
             <div class="card col-3" style="width: 18rem;">
                 <img class="card-img-top" src="<?php echo URLROOT; ?>/public/img/user.png" alt="users">
