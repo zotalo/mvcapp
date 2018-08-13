273186460bd3ba80475a876fb369697fc0d22033
zotalo <yagoukos@gmail.com>
2018-08-13 10:52:08 +0300

    users

--- app/views/administrators/index.php
+++ app/views/administrators/index.php
@@ -1,11 +1,23 @@
 <?php require APPROOT . '/views/inc/header.php';?>
 <div class="container">
-    <div class="card" style="width: 18rem;">
-        <img class="card-img-top" src="<?php echo URLROOT; ?>/public/img/user.png" alt="users">
-        <div class="card-body centered">
-         <h5 class="card-title">
-         Χρήστες</h5>
-         <a href="<?php echo URLROOT; ?>/administrators/users" class="btn btn-primary">Διαχείριση</a>
+    <div class="row mx-auto">
+        <div class="card" style="width: 18rem;">
+            <img class="card-img-top" src="<?php echo URLROOT; ?>/public/img/user.png" alt="users">
+            <div class="card-body mx-auto">
+                <a href="<?php echo URLROOT; ?>/administrators/users" class="btn btn-dark">Διαχείριση Χρηστών</a>
+            </div>
+        </div>
+        <div class="card" style="width: 18rem;">
+            <img class="card-img-top" src="<?php echo URLROOT; ?>/public/img/user.png" alt="users">
+            <div class="card-body mx-auto">
+                <a href="<?php echo URLROOT; ?>/administrators/users" class="btn btn-dark">Διαχείριση Χρηστών</a>
+            </div>
+        </div>
+        <div class="card" style="width: 18rem;">
+            <img class="card-img-top" src="<?php echo URLROOT; ?>/public/img/user.png" alt="users">
+            <div class="card-body mx-auto">
+                <a href="<?php echo URLROOT; ?>/administrators/users" class="btn btn-dark">Διαχείριση Χρηστών</a>
+            </div>
         </div>
     </div>
 </div>
