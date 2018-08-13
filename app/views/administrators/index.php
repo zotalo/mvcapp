10d0b19158db1546e0970ed21d60d6acd050e208
zotalo <yagoukos@gmail.com>
2018-08-13 10:35:16 +0300

    user

--- app/views/administrators/index.php
+++ app/views/administrators/index.php
@@ -1,11 +1,11 @@
 <?php require APPROOT . '/views/inc/header.php';?>
 <div class="container">
     <div class="card">
-        <img class="card-img-top" src="<?php echo APPROOT; ?>/public/img/user.png" alt="users">
+        <img class="card-img-top" src="<?php echo URLROOT; ?>/public/img/user.png" alt="users">
         <div class="card-body">
          <h5 class="card-title">
          Χρήστες</h5>
-         <a href="<?php echo URLROOT; ?>administrators/users" class="btn btn-primary"></a>
+         <a href="<?php echo URLROOT; ?>administrators/users" class="btn btn-primary">Διαχείριση</a>
         </div>
     </div>
 </div>
