c90eab52a1edd038b101940a2130352c3a3d30ee
zotalo <yagoukos@gmail.com>
2018-08-13 10:33:20 +0300

    users

--- app/views/administrators/index.php
+++ app/views/administrators/index.php
@@ -1,3 +1,12 @@
 <?php require APPROOT . '/views/inc/header.php';?>
-ADMINISTRATION
+<div class="container">
+    <div class="card">
+        <img class="card-img-top" src="<?php echo APPROOT; ?>/public/img/user.png" alt="users">
+        <div class="card-body">
+         <h5 class="card-title">
+         Χρήστες</h5>
+         <a href="<?php echo URLROOT; ?>administrators/users" class="btn btn-primary"></a>
+        </div>
+    </div>
+</div>
 <?php require APPROOT . '/views/inc/footer.php';?>
