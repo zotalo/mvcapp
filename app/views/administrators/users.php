e2960cc2106b61c841550129346b9fec5e76a418
zotalo <yagoukos@gmail.com>
2018-08-13 14:35:54 +0300

    users

--- app/views/administrators/users.php
+++ app/views/administrators/users.php
@@ -1,7 +1,7 @@
 <?php require APPROOT . '/views/inc/header.php';?>
 <div class="row mb-3">
     <div class="col-md-6">
-        <h1>Users</h1>
+        <h1>Χρήστες</h1>
     </div>
     <div class="col-md-6">
         <a href="<?php echo URLROOT; ?>/administrators/add" class="btn btn-primary pull-right">
