b44373218285f800687774beb5a62e3f820a8fcc
zotalo <yagoukos@gmail.com>
2018-08-13 11:02:07 +0300

    system

--- app/views/administrators/index.php
+++ app/views/administrators/index.php
@@ -1,24 +1,24 @@
 <?php require APPROOT . '/views/inc/header.php';?>
-<div class="container">
-    <div class="row mx-auto">
-        <div class="card" style="width: 18rem;">
-            <img class="card-img-top" src="<?php echo URLROOT; ?>/public/img/user.png" alt="users">
-            <div class="card-body mx-auto">
-                <a href="<?php echo URLROOT; ?>/administrators/users" class="btn btn-dark">Διαχείριση Χρηστών</a>
+    <div class="container text-center">
+        <div class="row justify-content-around">
+            <div class="card col-3" style="width: 18rem;">
+                <img class="card-img-top" src="<?php echo URLROOT; ?>/public/img/user.png" alt="users">
+                <div class="card-body mx-auto">
+                    <a href="<?php echo URLROOT; ?>/administrators/users" class="btn btn-dark">Διαχείριση Χρηστών</a>
+                </div>
             </div>
-        </div>
-        <div class="card" style="width: 18rem;">
-            <img class="card-img-top" src="<?php echo URLROOT; ?>/public/img/user.png" alt="users">
-            <div class="card-body mx-auto">
-                <a href="<?php echo URLROOT; ?>/administrators/users" class="btn btn-dark">Διαχείριση Χρηστών</a>
+            <div class="card col-3" style="width: 18rem;">
+                <img class="card-img-top" src="<?php echo URLROOT; ?>/public/img/folder.png" alt="users">
+                <div class="card-body mx-auto">
+                    <a href="<?php echo URLROOT; ?>/administrators/files" class="btn btn-dark">Διαχείριση Αρχείων</a>
+                </div>
             </div>
-        </div>
-        <div class="card" style="width: 18rem;">
-            <img class="card-img-top" src="<?php echo URLROOT; ?>/public/img/user.png" alt="users">
-            <div class="card-body mx-auto">
-                <a href="<?php echo URLROOT; ?>/administrators/users" class="btn btn-dark">Διαχείριση Χρηστών</a>
+            <div class="card col-3" style="width: 18rem;">
+                <img class="card-img-top" src="<?php echo URLROOT; ?>/public/img/system.png" alt="users">
+                <div class="card-body mx-auto">
+                    <a href="<?php echo URLROOT; ?>/administrators/system" class="btn btn-dark">Διαχείριση Συστήματος</a>
+                </div>
             </div>
         </div>
     </div>
-</div>
 <?php require APPROOT . '/views/inc/footer.php';?>
