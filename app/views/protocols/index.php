89e4ac44e6fc6f5f121458aca54ab0860384c49f
zotalo <yagoukos@gmail.com>
2018-08-16 14:34:07 +0300

    prot

--- app/views/protocols/index.php
+++ app/views/protocols/index.php
@@ -15,19 +15,19 @@
             <h4 class="card-title">
                 <?php echo $protocols->protocolYear. "." . $protocols->protocolNo . " - " . $protocols->inOutDescription;?>
             </h4>
-            <h5 class="card-title">
+            <h6 class="card-title">
             από χρήστη: <?php echo $protocols->username; ?>
-            </h5>
+            </h6>
             <div class="bg-light p2 mb-3">
-                Date: <?php echo $protocols->protocolDate; ?>
+                Ημ. Πρωτοκόλλου: <?php echo $protocols->protocolDate; ?>
             </div>
             <div class="bg-light p2 mb-3">
-                From/To: <?php echo $protocols->protocolFromTo; ?>
+                Από/Προς: <?php echo $protocols->protocolFromTo; ?>
             </div>
             <p class="card-text">
                 <?php echo $protocols->protocolDescription; ?>
             </p>
-            <a href="<?php echo URLROOT; ?>/protocol/show/<?php echo $post->postId; ?>" class="btn btn-dark">More</a>
+            <a href="<?php echo URLROOT; ?>/protocol/show/<?php echo $protocol->protocolId; ?>" class="btn btn-dark">Περισσότερα</a>
         </div>
     <?php endforeach; ?>
 <?php require APPROOT . '/views/inc/footer.php';?>
