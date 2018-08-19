bcb666cecbe9bcec6c3f50ee3afc71598e339789
zotalo <yagoukos@gmail.com>
2018-08-19 10:16:40 +0300

    hu

--- public/js/main.js
+++ public/js/main.js
@@ -6,7 +6,11 @@
 jQuery("document").ready(function($){
 
 	$(document).ready( function () {
-		$('#tprot').DataTable();
+		$('#tprot').DataTable({
+			"language": {
+                "url": "https://cdn.datatables.net/plug-ins/1.10.19/i18n/Greek.json"
+            }
+		});
 	} );
 
 	$(window).scroll(function () {
