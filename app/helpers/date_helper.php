9474adf31ef35bd18f0cbabcf1d457d793239c16
zotalo <yagoukos@gmail.com>
2018-08-20 11:38:46 +0300

    date

--- app/helpers/date_helper.php
+++ app/helpers/date_helper.php
@@ -2,12 +2,10 @@
 
 function dateFormat($date){
     $fdate = Datetime::createFromFormat('Y-m-d',  $date)->format('d/m/Y');
-    // $fdate = $date->format('d-m-Y H:i:s');
     return $fdate;
 }
 
 function dateFormatH($date){
     $fdate = Datetime::createFromFormat('Y-m-d H:i:s',  $date)->format('d/m/Y H:i:s');
-    // $fdate = $date->format('d-m-Y H:i:s');
     return $fdate;
 }
