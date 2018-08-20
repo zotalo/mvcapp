e2896b8bbd14fa32ae4ffcb85d65e3f760130ccd
zotalo <yagoukos@gmail.com>
2018-08-20 16:28:29 +0300

    add

--- app/controllers/Protocols.php
+++ app/controllers/Protocols.php
@@ -23,11 +23,40 @@ Class Protocols extends Controller {
     }
 
     public function add(){
-        //Get Protocols
+        if($_SERVER['REQUEST_METHOD'] == 'POST'){
+            //Sanitize POST array
+            $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+            
+            $data = [
+                'subject'=> trim($_POST['subject']),
+                'pdate'=> trim($_POST['pdate']),
+                'description'=> trim($_POST['description']),
+                'fromto' => trim($_POST['fromto']),
+                'nodoc' => trim($_POST['nodoc']),
+                'idate' => trim($_POST['idate']),
+                'file' => trim($_POST['file']),
+                'userId' => $_SESSION['user_id'],
+                'subject_err' => '',
+                'pdate_err' => '',
+                'fromto_err' => '',
+                'nodoc_err' => '',
+                'idate_err' => '',
+                'file_err' => ''
+            ];
+        
+        
+    } else {
         $data = [
-
+            'subject'=> '',
+            'pdate'=> '',
+            'description'=> '',
+            'fromto'=> '',
+            'nodoc'=> '',
+            'idate'=> '',
+            'file'=> '',
         ];
         $this->view('protocols/add', $data);
+        }
     }
 
     public function show($id){
--- app/views/protocols/add.php
+++ app/views/protocols/add.php
@@ -1,4 +1,46 @@
 <?php require APPROOT . '/views/inc/header.php';?>
 <a href="<?php echo URLROOT; ?>/protocols" class="btn btn-light"><i class="fa fa-backward"></i>Επιστροφή</a>
-<br>
+<div class="card card-body bg-light mt-5">
+            <h2>Νέο Πρωτόκολλο</h2>
+            <p>Δημιουργία</p>
+            <form action="<?php echo URLROOT; ?>/protocols/add" method="post">
+            <div class="form-group">
+                <label for="subject">Θέμα: <sup>*</sup></label>
+                <input type="text" name="subject" class="form-control form-control-lg <?php echo (!empty($data['subject_err'])) ? 'is-invalid' : ''; ?>" value="<?php echo $data['subject']; ?>">
+                <span class="invalid-feedback"><?php echo $data['subject_err']; ?></span>
+            </div>
+            <div class="form-group">
+                <label for="pdate">Ημερομηνία: <sup>*</sup></label>
+                <input type="date" name="pdate" data-date-format="dd/mm/yyyy" class="form-control form-control-lg <?php echo (!empty($data['pdate_err'])) ? 'is-invalid' : ''; ?>" value="<?php echo $data['pdate'];?>">
+                <span class="invalid-feedback"><?php echo $data['pdate_err']; ?></span>
+            </div>
+            <div class="form-group">
+                <label for="description">Περιγραφή: <sup>*</sup></label>
+                <textarea name="description" class="form-control form-control-lg <?php echo (!empty($data['description_err'])) ? 'is-invalid' : ''; ?>"><?php echo $data['description'];?></textarea>
+                <span class="invalid-feedback"><?php echo $data['description_err']; ?></span>
+            </div>
+            <div class="form-group">
+                <label for="fromto">Από/Προς: <sup>*</sup></label>
+                <input type="text" name="fromto" class="form-control form-control-lg <?php echo (!empty($data['fromto_err'])) ? 'is-invalid' : ''; ?>" value="<?php echo $data['fromto'];?>">
+                <span class="invalid-feedback"><?php echo $data['fromto_err']; ?></span>
+            </div>
+            <div class="form-group">
+                <label for="nodoc">Αρ. Εγγράφου: <sup>*</sup></label>
+                <input type="text" name="nodoc" class="form-control form-control-lg <?php echo (!empty($data['nodoc_err'])) ? 'is-invalid' : ''; ?>" value="<?php echo $data['nodoc'];?>">
+                <span class="invalid-feedback"><?php echo $data['nodoc_err']; ?></span>
+            </div>
+            <div class="form-group">
+                <label for="idate">Ημ. Έκδοσης: <sup>*</sup></label>
+                <input type="date" name="idate" data-date-format="dd/mm/yyyy" class="form-control form-control-lg <?php echo (!empty($data['idate_err'])) ? 'is-invalid' : ''; ?>" value="<?php echo $data['idate'];?>">
+                <span class="invalid-feedback"><?php echo $data['idate_err']; ?></span>
+            </div>
+            <div class="form-group">
+                <label for="file">Ημ. Έκδοσης: <sup>*</sup></label>
+                <input type="file" name="file" class="form-control form-control-lg <?php echo (!empty($data['file_err'])) ? 'is-invalid' : ''; ?>" value="<?php echo $data['file'];?>">
+                <span class="invalid-feedback"><?php echo $data['file_err']; ?></span>
+            </div>
+            <input type="submit" class="btn btn-success" value="Submit">
+            </form>
+        </div>
+    </div>
 <?php require APPROOT . '/views/inc/footer.php';?>
