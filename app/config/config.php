69123f715fe10c51aafc7df5e247b707a241cabc
[George.Siorokos] <[george.siorokos@soel.gr]>
2018-08-31 15:11:55 +0300

    local

--- app/config/config.php
+++ app/config/config.php
@@ -1,16 +1,16 @@
 <?php
 //DB_PARAMS
-define('DB_HOST', '192.168.0.131');
+define('DB_HOST', 'localhost');
 define('DB_USER', 'teadmin');
 define('DB_PASS', '~te@DBadm1n!');
 define('DB_NAME', 'tea_soeldb');
 
-//192.168.0.131
+//192.168.0.131 localhost
 
 //App Root
 define('APPROOT', dirname(dirname(__FILE__)));
 //Url Root
-define('URLROOT', 'http://192.168.0.131/teaprotocol');
+define('URLROOT', 'http://localhost/teaprotocol');
 //Site Name
 define('SITENAME', 'ΤΕΑ-ΣΟΕΛ');
 //App Version
