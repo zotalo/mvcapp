b23d95942f918325e9a75d817927144411c7f1dc
zotalo <yagoukos@gmail.com>
2018-09-13 12:33:25 +0300

    show

--- app/views/files/show.php
+++ app/views/files/show.php
@@ -1,6 +1,7 @@
 <?php require APPROOT . '/views/inc/header.php';?>
+<a href="<?php echo URLROOT; ?>/protocols/show<?php echo $id;?>" class="btn btn-light"><i class="fa fa-backward"></i>Επιστροφή</a>
+<br>
 <h1> <?php echo $data['name'];?> </h1>
-<h2> <?php echo $data['url'];?> </h2>
 <div class="embed-responsive embed-responsive-21by9">
 <iframe class="embed-responsive-item" src="<?php echo $data['url'];?>">
 
