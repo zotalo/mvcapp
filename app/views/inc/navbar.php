cce34cd7ab2ef339710873556525a1b11a7eef70
zotalo <yagoukos@gmail.com>
2018-09-13 12:31:01 +0300

    arxeia

--- app/views/inc/navbar.php
+++ app/views/inc/navbar.php
@@ -15,9 +15,9 @@
               <li class="nav-item"><a class="nav-link" href="<?php echo URLROOT; ?>/protocols/all">Όλα Τα Έτη</a></li>
             </ul>
           </li>
-          <li class="nav-item">
+          <!-- <li class="nav-item">
             <a class="nav-link" href="<?php echo URLROOT; ?>/files/index">Αρχεία</a>
-          </li>
+          </li> -->
           <li class="nav-item">
             <a class="nav-link" href="<?php echo URLROOT; ?>/pages/about">Σχετικά</a>
           </li>
