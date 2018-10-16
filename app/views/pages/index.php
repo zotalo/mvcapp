66fd3d3228f3d9aae738b503f17b3e18a4dd896b
[George.Siorokos] <[george.siorokos@soel.gr]>
2018-10-16 09:25:10 +0300

    index

--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -3,6 +3,5 @@
         <h1 class="display-3"><?php echo $data['title'];?></h1>
         <p class="lead"><?php echo $data['description'];?></p>
         <div class="lead"><img src="<?php echo URLROOT;?>/img/tea.png" alt="tea" /></div>
-        <?php echo "UPLOADS: ".UPLOADS ."<br> APPROOT: ".APPROOT."<br> URLROOT: ".URLROOT."<br> UPFOLD: ".UPFOLD; ?>
     </div>
 <?php require APPROOT . '/views/inc/footer.php';?>
