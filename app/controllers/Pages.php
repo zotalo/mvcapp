42a203e6d7b97591f55b4853d514cbb4c5f9ecec
zotalo <yagoukos@gmail.com>
2018-11-11 13:39:51 +0200

    smallchanges

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -1,16 +1,5 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- * Description of Pages
- *
- * @author Administrator
- */
 class Pages extends Controller{
    public function __construct() {
     if(!isLoggedIn()){
