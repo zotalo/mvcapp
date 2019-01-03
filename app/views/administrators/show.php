3d9eb39cc446878ec682ce265cd16ae9a82e1c7b
[George.Siorokos] <[george.siorokos@soel.gr]>
2019-01-03 15:28:29 +0200

    show_admin

--- app/views/administrators/show.php
+++ app/views/administrators/show.php
@@ -1,5 +1,8 @@
 <?php require APPROOT . '/views/inc/header.php';?>
-
-<h1> TEST </h1>
-
+<?php flash('administrator_message');?>
+<a href="<?php echo URLROOT; ?>/administrators/users" class="btn btn-light"><i class="fa fa-backward"></i>Επιστροφή</a>
+<h1> <?php echo $data['users']->userName;?> </h1>
+<p><strong>Δικαιώματα Χρήστη: </strong><?php echo $data['users']->rolesDescription;?></p>
+<p><strong>Κατάσταση Χρήστη: </strong><?php echo $data['users']->statusDescription;?></p>
+<p><strong>Email: </strong><?php echo $data['users']->email;?></p>
 <?php require APPROOT . '/views/inc/footer.php';?>
