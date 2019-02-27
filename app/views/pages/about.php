c1f74f10d59b820c34e1dffdd4ecdb583524f549
[George.Siorokos] <[george.siorokos@soel.gr]>
2019-02-27 15:49:48 +0200

    versions

--- app/views/pages/about.php
+++ app/views/pages/about.php
@@ -1,8 +1,9 @@
 <?php require APPROOT . '/views/inc/header.php';?>
 <h1><?php echo $data['title'];?></h1>
 <p><?php echo $data['description'];?></p>
-<p>Έκδοση: <strong><?php echo APPVERSION; ?></strong></p>
+<p>Τελαυταία Έκδοση: <strong><?php echo APPVERSION; ?></strong></p>
 <p>Αλλαγές Στην Έκδοση: <strong><?php echo APPCHANGES; ?></strong></p>
+<p>Ιστορικό Εκδόσεων: <strong><a href="<?php echo URLROOT;?>/pages/versions.php">Όλες οι εκδόσεις</a></strong>
 <p>Υπεύθυνος Ανάπτυξης: <strong><?php echo APPDEVELOPER; ?></strong></p>
 <p>Επικοινωνία: Email. <strong><a href="mailto:<?php echo DEVELOPEREMAIL;?>"><?php echo DEVELOPEREMAIL;?></a></strong> τηλ. <strong><a href="tel:2103891494"><?php echo DEVELOPERPHONE;?></a></strong>
 <?php require APPROOT . '/views/inc/footer.php';?>
