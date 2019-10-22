e45ff4c0ff23a1b4c3f233328ded6dc2f16274bd
[George.Siorokos] <[george.siorokos@soel.gr]>
2019-10-22 07:49:31 +0300

    logout_900s

--- app/views/inc/header.php
+++ app/views/inc/header.php
@@ -4,6 +4,7 @@
         <meta charset="UTF-8">
         <meta name ="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <meta http-equiv="refresh" content="900;url=<?php echo URLROOT; ?>/users/logout" />
         <link rel="icon" href="<?php echo URLROOT; ?>/img/favicon.ico" type="image/x-icon"/>
         <link rel ="stylesheet" href="<?php echo URLROOT; ?>/css/style.css">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
